784bd39d7c9dd0ef37721ef5128b8c30e5959106
Tristan Slominski <tristan.slominski@gmail.com>
2012-12-12 20:50:48 -0600

    worker environment and test helpers are partially tested

---
+++ test/examples.js
@@ -0,0 +1,23 @@
+var examples = [
+  'examples/example-scopeObject/test/scopeObject',
+  'examples/testing-callCallback/test/callCallback',
+  'examples/testing-callErrorCallback/test/callErrorCallback',
+  'examples/testing-dontMockHttp/test/dontMockHttp',
+  'examples/testing-dontMockHttps/test/dontMockHttps',
+  'examples/testing-httpRequestFor/test/httpRequestFor',
+  'examples/testing-httpResponseFor/test/httpResponseFor',
+  //'examples/testing-proxy/test/proxy', // requires crosstalk token
+  'examples/testing-sendHttpResponseTo/test/sendHttpResponseTo',
+  'examples/testing-shouldBeSilent/test/shouldBeSilent',
+  'examples/testing-shouldBeSilentFromNowOn/test/shouldBeSilentFromNowOn',
+  'examples/testing-shouldCallCallback/test/shouldCallCallback',
+  'examples/testing-shouldCallErrorCallback/test/shouldCallErrorCallback',
+  'examples/testing-shouldEmit/test/shouldEmit',
+  'examples/testing-shouldEmitNew/test/shouldEmitNew',
+  'examples/testing-shouldNotEmit/test/shouldNotEmit',
+  'examples/testing-version/test/version'
+];
+
+examples.forEach( function ( example ) {
+  require( '../' + example );
+});
